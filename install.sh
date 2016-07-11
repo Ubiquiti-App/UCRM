ce3a8d3ed1b61a96eabfae0a3f909996ad8af676
Andrej Poliak <andrej.poliak@ubnt.com>
2016-07-11 17:02:38 +0200

    Added support for SSL

--- install.sh
+++ install.sh
@@ -86,7 +86,7 @@ install_docker_compose() {
 	which docker-compose > /dev/null 2>&1
 
 	if [ $? = 1 ]; then
-		echo "Download and install Docker compose"
+		echo "Download and install Docker compose."
 		curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 		chmod +x /usr/local/bin/docker-compose
 	fi
@@ -101,7 +101,7 @@ install_docker_compose() {
 
 create_user() {
 	if [ -z "$(getent passwd $UCRM_USER)" ]; then
-		echo "Creating user $UCRM_USER"
+		echo "Creating user $UCRM_USER."
 		adduser --disabled-password --gecos "" $UCRM_USER
 		usermod -aG docker $UCRM_USER
 	fi
@@ -109,21 +109,22 @@ create_user() {
 
 download_docker_compose_files() {
 	if [ ! -f /home/$UCRM_USER/docker-compose.yml ]; then
-		echo "Downloading docker compose files"
+		echo "Downloading docker compose files."
 		curl -o /home/$UCRM_USER/docker-compose.yml https://raw.githubusercontent.com/U-CRM/billing/master/docker-compose.yml
 		curl -o /home/$UCRM_USER/docker-compose.env https://raw.githubusercontent.com/U-CRM/billing/master/docker-compose.env
 
-		echo "Replacing path in docker compose"
+		echo "Replacing path in docker compose."
 		sed -i -e "s/#volumes:/volumes:/g" /home/$UCRM_USER/docker-compose.yml
 		sed -i -e "s/#  \- \/home\/docker\/ucrm\/postgres:\/var\/lib\/postgresql\/data/  - \/home\/$UCRM_USER\/postgres:\/var\/lib\/postgresql\/data/g" /home/$UCRM_USER/docker-compose.yml
 		sed -i -e "s/#  \- \/home\/docker\/ucrm:\/data/  - \/home\/$UCRM_USER\/data:\/data/g" /home/$UCRM_USER/docker-compose.yml
 
-		echo "Replacing env in docker compose"
+		echo "Replacing env in docker compose."
 		sed -i -e "s/POSTGRES_PASSWORD=ucrmdbpass1/POSTGRES_PASSWORD=$POSTGRES_PASSWORD/g" /home/$UCRM_USER/docker-compose.env
 		sed -i -e "s/SECRET=changeThisSecretKey/SECRET=$SECRET/g" /home/$UCRM_USER/docker-compose.env
 
 		change_ucrm_port
 		change_ucrm_suspend_port
+		enable_ssl
 	fi
 }
 
@@ -136,12 +137,12 @@ change_ucrm_port() {
 		case $PORT in
 			[yY][eE][sS]|[yY])
 				sed -i -e "s/- 8080:80/- 80:80/g" /home/$UCRM_USER/docker-compose.yml
-				echo "U CRM will start at 80 port"
+				echo "UCRM will start at 80 port."
 				echo "#used only in instalation" >> /home/$UCRM_USER/docker-compose.env
 				echo "SERVER_PORT=80" >> /home/$UCRM_USER/docker-compose.env
 				break;;
 			[nN][oO]|[nN])
-				echo "U CRM will start at 8080 port. If you will change it, edit your docker-compose.yml in $UCRM_USER home direcotry."
+				echo "UCRM will start at 8080 port. If you will change it, edit your docker-compose.yml in $UCRM_USER home direcotry."
 				echo "#used only in instalation" >> /home/$UCRM_USER/docker-compose.env
 				echo "SERVER_PORT=8080" >> /home/$UCRM_USER/docker-compose.env
 				break;;
@@ -160,12 +161,12 @@ change_ucrm_suspend_port() {
 		case $PORT in
 			[yY]*)
 				sed -i -e "s/- 8081:81/- 81:81/g" /home/$UCRM_USER/docker-compose.yml
-				echo "U CRM suspend page will start at 81 port"
+				echo "UCRM suspend page will start at 81 port."
 				echo "#used only in instalation" >> /home/$UCRM_USER/docker-compose.env
 				echo "SERVER_SUSPEND_PORT=81" >> /home/$UCRM_USER/docker-compose.env
 				break;;
 			[nN]*)
-				echo "U CRM suspend page will start at 8081 port. If you will change it, edit your docker-compose.yml in $UCRM_USER home direcotry."
+				echo "UCRM suspend page will start at 8081 port. If you will change it, edit your docker-compose.yml in $UCRM_USER home direcotry."
 				echo "#used only in instalation" >> /home/$UCRM_USER/docker-compose.env
 				echo "SERVER_SUSPEND_PORT=8081" >> /home/$UCRM_USER/docker-compose.env
 				break;;
@@ -175,13 +176,60 @@ change_ucrm_suspend_port() {
 	done
 }
 
+enable_ssl() {
+	local SSL
+
+	while true; do
+		read -r -p "Do you want to enable SSL? (You need to generate a certificate for yourself) [Y/n]: " SSL
+
+		case $SSL in
+			[yY]*)
+				enable_server_name
+				change_ucrm_ssl_port
+				break;;
+			[nN]*)
+				echo "UCRM has disabled support for SSL."
+				break;;
+			*)
+				;;
+		esac
+	done
+}
+
+enable_server_name() {
+	local SERVER_NAME
+
+	read -r -p "Enter Server domain name for UCRM, for example ucrm.example.com: " SERVER_NAME
+	echo "SERVER_NAME=$SERVER_NAME" >> /home/$UCRM_USER/docker-compose.env
+}
+
+change_ucrm_ssl_port() {
+	local PORT
+
+	while true; do
+		read -r -p "Do you want UCRM SSL to be accessible on port 443? (Yes: recommended for most users, No: will set 8443 as default) [Y/n]: " PORT
+
+		case $PORT in
+			[yY]*)
+				sed -i -e "s/- 8443:443/- 443:443/g" /home/$UCRM_USER/docker-compose.yml
+				echo "UCRM SSL will start at 443 port."
+				break;;
+			[nN]*)
+				echo "UCRM SSL will start at 8443 port."
+				break;;
+			*)
+				;;
+		esac
+	done
+}
+
 download_docker_images() {
-	echo "Downloading docker images"
+	echo "Downloading docker images."
 	cd /home/$UCRM_USER && /usr/local/bin/docker-compose pull
 }
 
 start_docker_images() {
-	echo "Starting docker images"
+	echo "Starting docker images."
 	cd /home/$UCRM_USER && /usr/local/bin/docker-compose up -d && /usr/local/bin/docker-compose ps
 }
 
