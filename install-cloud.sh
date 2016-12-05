2083c2e6d7c8367d7aaa2cd4aa1e6c9373e8606e
UBNT-Petr <petr.vavra@ubnt.com>
2016-12-05 15:10:07 +0100

    install cloud script

---
+++ install-cloud.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script will install ucrm on a cloud hosted server without any promt
+# Standard ports 80,81,443 will be used
+# This script requires the variable SERVER_NAME to be defined
+
+INSTALL_CLOUD=true
+if [ -z "$SERVER_NAME" ]; then SERVER_NAME=""; fi
+
+. ./install.sh
