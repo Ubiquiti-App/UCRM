df13b6e51dbb19a47a262e0a17c4d975cc690491
UBNT-Petr <petr.vavra@ubnt.com>
2017-04-20 09:46:44 +0200

    swap setup in separate sh script

--- install-cloud.sh
+++ install-cloud.sh
@@ -7,20 +7,8 @@
 INSTALL_CLOUD=true
 CLOUD_CONF="/tmp/cloud_conf"
 
-# Set up 2G swap file (optimized for Ubuntu server on DigitalOcean, 1GB RAM, 20GB disk)
-# See https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-ubuntu-14-04
-echo "Creating swap file"
-fallocate -l 2G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
-sysctl vm.swappiness=10
-echo "vm.swappiness=10" >> /etc/sysctl.conf
-sysctl vm.vfs_cache_pressure=50
-echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
-swapon -s
-echo "swap file created"
+# Set up swap file
+. /tmp/setup-swap.sh
 
 # Install UCRM
 . /tmp/install.sh
